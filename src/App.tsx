badb8ad8e40603b16a3a38a0eea256046e686845
Ernest Bruno <erndobruno@gmail.com>
2022-04-06 14:53:10 -0700

    Create a Todo model/interface, set up another state slice for storing todos in an array, and create a function for handling form submit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './model';
 
 
 const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+  }
 
   return (
     <div className="App">
       <span className="heading">Tasky</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
