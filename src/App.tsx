b05ca85771dec895f32ae1959ae52cce8f10c8e4
Ernest Bruno <erndobruno@gmail.com>
2022-04-06 14:58:42 -0700

    Works on form submission.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React, { useState } from 'react';
-import './App.css';
-import InputField from './components/InputField';
-import { Todo } from './model';
-
+import { stringify } from "querystring";
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
+    const [todo, setTodo] = useState<string>('');
+    const [todos, setTodos] = useState<Todo[]>([]);
 
-  const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
+    const handleAdd = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (todo) {
+            setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+            setTodo('');
+        }
+    };
 
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault();
-  }
-
-  return (
-    <div className="App">
-      <span className="heading">Tasky</span>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <span className="heading">Tasky</span>
+            <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        </div>
+    );
+};
 
 export default App;
