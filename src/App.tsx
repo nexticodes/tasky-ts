50e0c6cf2365317ae9bc57f4efd66767f1b2d568
Ernest Bruno <erndobruno@gmail.com>
2022-04-06 12:43:04 -0700

    Introduce todo state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
 
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string | number>("");
+
   return (
     <div className="App">
-      <span className="heading">Taskify</span>
-      <InputField />
+      <span className="heading">Tasky</span>
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
 }
