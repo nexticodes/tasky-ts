f790dff881a2974137b4ebdf879a33c8f9c7c139
Ernest Bruno <erndobruno@gmail.com>
2022-04-07 15:30:32 -0700

    Start working on TodoReducer.

--- src/model.ts
+++ src/model.ts
@@ -3,3 +3,34 @@ export interface Todo {
     todo: string;
     isDone: boolean;
 }
+
+type Actions =
+    | { type: "add"; payload: string }
+    | { type: "remove"; payload: number }
+    | { type: "done"; payload: number };
+
+export const TodoReducer = (state: Todo[], action: Actions) => {
+    switch(action.type) {
+        case "add":
+            return [
+                ...state,
+                {
+                    id: Date.now(),
+                    todo: action.payload,
+                    isDone: false
+                }
+            ];
+        case "remove":
+            return state.filter(t => t.id !== action.payload);
+        case "done":
+            return state.map(t => t.id !== action.payload ? {
+                ...t, isDone: !t.isDone
+            }: t)
+    }
+};
+
+// import { useReducer } from "react";
+// const ReducerExample = () => {
+//     const [state, dispatch] = useReducer(TodoReducer, []);
+//     return <div>model</div>;
+// };
