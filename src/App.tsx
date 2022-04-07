287eafd88fa0a2a9d81deb801ac1794e20ae1b08
Ernest Bruno <erndobruno@gmail.com>
2022-04-07 14:14:32 -0700

    Import TodoList and pass appropriate props.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { stringify } from "querystring";
 import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
 import { Todo } from "./model";
 
 const App: React.FC = () => {
@@ -20,9 +20,7 @@ const App: React.FC = () => {
         <div className="App">
             <span className="heading">Tasky</span>
             <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-            {todos.map((t) => (
-              <li>{t.todo}</li>
-            ))}
+            <TodoList todos={todos} setTodos={setTodos} />
         </div>
     );
 };
